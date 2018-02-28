c6c98ee3291a140e19fae923e20db87dfc507939
Arti2189 <30336380+Arti2189@users.noreply.github.com>
2018-02-28 22:57:50 +0530

    Mainactivity

---
+++ app/src/main/java/com/example/sudhir/design/recommendations.java
@@ -0,0 +1,13 @@
+package com.example.sudhir.design;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class recommendations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendations);
+    }
+}
