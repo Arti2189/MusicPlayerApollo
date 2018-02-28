6acfe655572ffef52e5705f494493a05e316e129
Arti2189 <30336380+Arti2189@users.noreply.github.com>
2018-02-28 14:19:07 +0530

    download file commit

---
+++ app/src/main/java/com/example/sudhir/design/downloads.java
@@ -0,0 +1,25 @@
+package com.example.sudhir.design;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button
+/**
+ * Created by ADMIN on 2/28/2018.
+ */
+
+public class downloads extends AppCompatActivity{
+    public Button downloads;
+
+    public void init(){
+        downloads = (Button)findViewById(R.id.downloads);
+        downloads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent downloading = new Intent(downloads.this.)
+            }
+        });
+    }
+
+
+}
