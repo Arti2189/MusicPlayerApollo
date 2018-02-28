112b8032507a0126ad2970b04239c68144314212
Arti2189 <30336380+Arti2189@users.noreply.github.com>
2018-02-28 22:49:46 +0530

    downloads

--- app/src/main/java/com/example/sudhir/design/downloads.java
+++ app/src/main/java/com/example/sudhir/design/downloads.java
@@ -1,26 +1,13 @@
 package com.example.sudhir.design;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button
-/**
- * Created by ADMIN on 2/28/2018.
- */
+import android.os.Bundle;
 
-public class downloads extends AppCompatActivity{
-    public Button downloads;
+public class downloads extends AppCompatActivity {
 
-    public void init(){
-        downloads = (Button)findViewById(R.id.downloads);
-        downloads.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent downloading = new Intent(downloads.this.)
-dsssssssssssssssss
-            }
-        });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_downloads);
     }
-
-
 }
